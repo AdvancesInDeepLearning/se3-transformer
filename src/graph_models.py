7211557d384a4bebecfabc3db2d4e4cb410df866
arundoKo3n1g <oliver.koenig.intern@arundo.com>
2021-03-10 12:15:43 +0100

    Working on DGL to GraphIndependent input

--- src/graph_models.py
+++ src/graph_models.py
@@ -3,16 +3,24 @@
 * Or an even sweeter SE3-Transformer GCN?
 * Let's go wild...
 """
-
+import dgl
 import torch.nn as nn
+from torch.autograd import Variable
 
 
 class DGLGraphIndependent(nn.Module):
-    def __init__(self,
-                 edge_model_fn=None,
-                 node_model_fn=None,
-                 global_model_fn=None,
-                 ):
+    """
+    The inputs and outputs are graphs. The corresponding models are applied to
+    each element of the graph (edges, nodes and globals) in parallel and
+    independently of the other elements. It can be used to encode or
+    decode the elements of a graph.
+    """
+
+    def __init__(
+        self,
+        edge_model_fn: nn.Module = None,
+        node_model_fn: nn.Module = None,
+    ):
         super().__init__()
 
         if edge_model_fn is None:
@@ -25,19 +33,30 @@ class DGLGraphIndependent(nn.Module):
         else:
             self._node_model = node_model_fn
 
-        if global_model_fn is None:
-            self._global_model = lambda x: x
-        else:
-            self._global_model = global_model_fn
-
-    def forward(self, graph,
-             edge_model_kwargs=None,
-             node_model_kwargs=None,
-             global_model_kwargs=None):
-        pass
+    def forward(self, graph: dgl.DGLGraph) -> dgl.DGLGraph:
+        X = graph.ndata
+        self._node_model(X)
+        graph.apply_edges(self._edge_model)
+        return graph
 
 
 class DGLInteractionNetwork(nn.Module):
-    def __init__(self):
+    """
+    An interaction networks computes interactions on the edges based on the
+    previous edges features, and on the features of the nodes sending into those
+    edges. It then updates the nodes based on the incomming updated edges.
+    """
+
+    def __init__(self, edge_model_fn: nn.Module, node_model_fn: nn.Module):
         super(DGLInteractionNetwork, self).__init__()
-        pass
+
+        self._edge_block = blocks.EdgeBlock(
+            edge_model_fn=edge_model_fn, use_globals=False
+        )
+
+        self._node_block = blocks.NodeBlock(
+            node_model_fn=node_model_fn,
+            use_sent_edges=False,
+            use_globals=False,
+            received_edges_reducer=reducer,
+        )
