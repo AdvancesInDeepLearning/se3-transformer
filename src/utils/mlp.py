9ff84899f0db14286072a0e782fa0a09672518a9
arundoKo3n1g <oliver.koenig.intern@arundo.com>
2021-03-10 11:17:02 +0100

    Adding MLP next to LayerNormMLP

--- src/utils/mlp.py
+++ src/utils/mlp.py
@@ -1,17 +1,24 @@
 import torch.nn as nn
 
 
-class LayerNormMLP(nn.Module):
+class MLP(nn.Module):
     def __init__(self, hidden_size, num_layers, latent_size):
-        super(LayerNormMLP, self).__init__()
+        super(MLP, self).__init__()
 
         self.layers = []
-        for i in range(num_layers-1):
-            self.layers.append(nn.Linear(in_features=hidden_size, out_features=hidden_size))
+        for i in range(num_layers - 1):
+            self.layers.append(
+                nn.Linear(in_features=hidden_size, out_features=hidden_size)
+            )
         self.layers.append(nn.Linear(in_features=hidden_size, out_features=latent_size))
-        self.layers.append(nn.LayerNorm(normalized_shape=latent_size))
 
     def forward(self, x):
         for i, _ in enumerate(self.layers):
             x = self.layers[i](x)
         return x
+
+
+class LayerNormMLP(MLP):
+    def __init__(self, hidden_size, num_layers, latent_size):
+        super(LayerNormMLP, self).__init__(hidden_size, num_layers, latent_size)
+        self.layers.append(nn.LayerNorm(normalized_shape=latent_size))
